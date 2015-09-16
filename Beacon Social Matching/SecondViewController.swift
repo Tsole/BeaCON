6943704e95a680f214c783a78f78fbc47f3fee33
Konstantinos Tsoleridis <ktsoleridis@gmail.com>
2015-09-16 11:51:36 +0200

    Initial Commit

---
+++ Beacon Social Matching/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Beacon Social Matching
+//
+//  Created by Tsole on 16/9/15.
+//  Copyright © 2015 Konstantinos Tsoleridis. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
